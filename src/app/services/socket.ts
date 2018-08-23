72a21cd3e89a4e09b65bc8cb5fd2e2653b68e80f
jrs112 <jrjackets112@gmail.com>
2018-08-22 20:45:09 -0400

    update socket url again to attempt deployment bug fix

--- src/app/services/socket.ts
+++ src/app/services/socket.ts
@@ -11,7 +11,7 @@ export class SocketService {
 private socket;
 
 constructor(private http: HttpClient) {
-  const serverUrl = "http://localhost:4200";
+  const serverUrl = "passportchallenge.herokuapp.com";
   this.socket = io(serverUrl);
 }
 
